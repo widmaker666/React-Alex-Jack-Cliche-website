17c27e757982927265f746ef81051daea299d1dc
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-07-29 17:35:29 +0200

    cursor effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,73 +2,77 @@ import backgroundHome from "../assets/images/blackLight1.jpg";
 import { useState } from "react";
 
 const Home = () => {
+  //-Constants //
   const [mouseMoveY, setMouseMoveY] = useState();
   const [mouseMoveX, setMouseMoveX] = useState();
+  const circle = document.querySelector(".circle");
 
-  const mouseMove = document.querySelectorAll(".circle");
+  //-Functions MouseMove //
+  function mouseMoveCircle1(e) {
+    setMouseMoveX(e.clientX);
+    setMouseMoveY(e.clientY);
+    circle.style.left = mouseMoveX + "px";
+    circle.style.top = mouseMoveY + "px";
+  }
+  //-Function MouseEnter //
+  function moveEnterFirstTitle() {
+    circle.style.zIndex = "0";
+    circle.style.background = "cyan";
+    circle.style.transition = "0.05s ease";
+    circle.style.filter = "blur(10px)";
+    circle.style.width = "120px";
+    circle.style.height = "120px";
+  }
 
-  function mouseMoveEvent(e) {
-    setMouseMoveX(e.pageX);
-    setMouseMoveY(e.pageY);
-    mouseMove.forEach((circle) => {
-      mouseMove.style.left = mouseMoveX + "px";
-      mouseMove.style.top = mouseMoveY + "px";
-    });
+  //- Function MouseLeave //
+  function moveLeaveFirstTitle() {
+    circle.style.background = "red";
+    circle.style.filter = "invert(0)";
+    circle.style.transition = ".05s ease";
+    circle.style.width = "100px";
+    circle.style.height = "100px";
   }
 
   return (
-    <main className="main" onMouseMove={mouseMoveEvent}>
+    <main className="main">
       <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <section className="section-home">
+
+      <section className="section-home" onMouseMove={mouseMoveCircle1}>
         <div className="img-div">
           <img src={backgroundHome} alt="" />
         </div>
-        <section className="title-container">
-          <div className="first-title">
-            <span>a</span>
-            <span>l</span>
-            <span>e</span>
-            <span>x</span>
-            <span>a</span>
-            <span>n</span>
-            <span>d</span>
-            <span>r</span>
-            <span>e</span>
-          </div>
-          <div className="under-title">
-            <h2>professionnal photographer passioned</h2>
-          </div>
-          <div className="second-title">
-            <span>j</span>
-            <span>a</span>
-            <span>c</span>
-            <span>k</span>
-          </div>
-          <div className="third-title">
-            <span>c</span>
-            <span>l</span>
-            <span>i</span>
-            <span>c</span>
-            <span>h</span>
-            <span>é</span>
-          </div>
-        </section>
-        <div className="enter">
-          <span id="one">e</span>
-          <span id="two">n</span>
-          <span id="three">t</span>
-          <span id="four">e</span>
-          <span id="five">r</span>
+
+        <div
+          className="first-title"
+          onMouseEnter={moveEnterFirstTitle}
+          onMouseLeave={moveLeaveFirstTitle}
+        >
+          <span>a</span>
+          <span>l</span>
+          <span>e</span>
+          <span>x</span>
+          <span>a</span>
+          <span>n</span>
+          <span>d</span>
+          <span>r</span>
+          <span>e</span>
+        </div>
+        <div className="under-title">
+          <h2>professionnal photographer passioned</h2>
+        </div>
+        <div className="second-title">
+          <span>j</span>
+          <span>a</span>
+          <span>c</span>
+          <span>k</span>
+        </div>
+        <div className="third-title">
+          <span>c</span>
+          <span>l</span>
+          <span>i</span>
+          <span>c</span>
+          <span>h</span>
+          <span>é</span>
         </div>
       </section>
     </main>
